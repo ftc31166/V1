ade7374abe09532aea3f52e19c6e88cc7ef4713e
AdityaVsVik <typius.gamer@gmail.com>
2025-10-12 17:23:24 -0500

    Made motor test use 2 motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/MotorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/MotorTest.java
@@ -13,31 +13,39 @@ public class MotorTest extends LinearOpMode {
         DcMotor motor1 = hardwareMap.get(DcMotor.class,"motor1");
         ElapsedTime timer = new ElapsedTime();
         if (isStopRequested()) return;
-        double power = 0;
+        double powerOut = 0;
+        double powerIn = 0;
         waitForStart();
         while (opModeIsActive()){
             if(gamepad1.a){
-                motor.setPower(power);
+                motor.setPower(powerOut);
             }if(gamepad1.b){
                 motor.setPower(0);
             }
             if(gamepad1.dpad_right && timer.milliseconds()>300){
-               power += .1;
+               powerOut += .1;
                timer.reset();
             }
             if(gamepad1.dpad_left && timer.milliseconds()>300){
-                power -= .1;
+                powerOut -= .1;
                 timer.reset();
             }
             if (gamepad1.x){
-                motor1.setPower(power);
+                motor1.setPower(powerIn);
             }
             if (gamepad1.y){
                 motor1.setPower(0);
             }
-
-
-            telemetry.addData("Motor Power", power);
+            if (gamepad1.dpad_up && timer.milliseconds()>300){
+                powerIn += .1;
+                timer.reset();
+            }
+            if (gamepad1.dpad_down  && timer.milliseconds()>300){
+                powerIn -= .1;
+                timer.reset();
+            }
+            telemetry.addData("Outtake Motor power", powerOut);
+            telemetry.addData("Intake Motor power", powerIn);
             telemetry.update();
         }
     }
