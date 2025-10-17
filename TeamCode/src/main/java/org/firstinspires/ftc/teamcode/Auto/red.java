7d7300e0bd76c4cc0c10ba9715e625533e96f986
Dhee0104 <168314704+Dhee0104@users.noreply.github.com>
2025-10-17 18:53:21 -0500

    Update red.java
    Added functions and tag detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/red.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.teleops;
+package org.firstinspires.ftc.teamcode.Auto;
 import androidx.annotation.NonNull;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
@@ -12,6 +12,8 @@ import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.teamcode.Subsystems.Constants;
 import org.firstinspires.ftc.teamcode.moreTuning.PinpointDrive;
 import org.firstinspires.ftc.teamcode.Subsystems.AutonFuncs;
 
@@ -42,52 +44,67 @@ public class red extends LinearOpMode {
 
 
 
-        TrajectoryActionBuilder tab2 = drive.actionBuilder(new Vector2d(-10,11))
-                .lineToY(37)
-                .setTangent(Math.toRadians(0))
-                .lineToX(18)
-                .waitSeconds(3)
-                .setTangent(Math.toRadians(0))
-                .lineToXSplineHeading(46, Math.toRadians(180))
-                .waitSeconds(3);
-        TrajectoryActionBuilder tab3 = drive.actionBuilder(new Vector2d(-10,11))
-                .lineToYSplineHeading(33, Math.toRadians(180))
-                .waitSeconds(2)
-                .strafeTo(new Vector2d(46, 30))
-                .waitSeconds(3);
+        TrajectoryActionBuilder GPP_PPG = drive.actionBuilder(new Pose2d(-10,11,Math.toRadians(90)))
+                .stopAndAdd(robot.intakeOn())
+                .setTangent(Math.toRadians(-10))
+                .splineToConstantHeading(new Vector2d(35,50),Math.toRadians(90))
+                .stopAndAdd(new SleepAction(1))
+                .stopAndAdd(robot.intakeOff())
+                .strafeTo(new Vector2d(-10,11))
+                .stopAndAdd(robot.shooterOn())
+                .stopAndAdd(new SleepAction(2))
+                .stopAndAdd(robot.shooterOff())
+                .stopAndAdd(robot.intakeOn())
+                .strafeTo(new Vector2d(-10,50))
+                .stopAndAdd(new SleepAction(1))
+                .stopAndAdd(robot.intakeOff())
+                .strafeTo(new Vector2d(-10,11))
+                .stopAndAdd(robot.shooterOn())
+                .stopAndAdd(new SleepAction(2))
+                .stopAndAdd(robot.shooterOff())
+                .setTangent(Math.toRadians(45))
+                .splineToConstantHeading(new Vector2d(0,50),Math.toRadians(90));
+        TrajectoryActionBuilder PGP= drive.actionBuilder(new Pose2d(-10,11,Math.toRadians(90)))
+                .stopAndAdd(robot.intakeOn())
+                .strafeTo(new Vector2d(-10,50))
+                .stopAndAdd(new SleepAction(1))
+                .stopAndAdd(robot.intakeOff())
+                .strafeTo(new Vector2d(-10,11))
+                .stopAndAdd(robot.shooterOn())
+                .stopAndAdd(new SleepAction(2))
+                .stopAndAdd(robot.shooterOff())
+                .setTangent(0)
+                .stopAndAdd(robot.intakeOn())
+                .splineToConstantHeading(new Vector2d(10,50),Math.toRadians(90))
+                .stopAndAdd(new SleepAction(1))
+                .stopAndAdd(robot.intakeOff())
+                .strafeTo(new Vector2d(-10,11))
+                .stopAndAdd(robot.shooterOn())
+                .stopAndAdd(new SleepAction(2))
+                .stopAndAdd(robot.shooterOff())
+                .setTangent(Math.toRadians(45))
+                .splineToConstantHeading(new Vector2d(0,50),Math.toRadians(90));
 
 
         // actions that need to happen on init; for instance, a claw tightening.
-        Actions.runBlocking(claw.closeClaw());
 
-
-
-
-        int startPosition = visionOutputPosition;
-        telemetry.addData("Starting Position", startPosition);
-        telemetry.update();
         waitForStart();
 
         if (isStopRequested()) return;
 
-        Action trajectoryActionChosen;
-        if (startPosition == 1) {
-            trajectoryActionChosen = tab1.build();
-        } else if (startPosition == 2) {
-            trajectoryActionChosen = tab2.build();
-        } else {
-            trajectoryActionChosen = tab3.build();
-        }
+
 
         Actions.runBlocking(
                 new SequentialAction(
-                        trajectoryActionChosen,
-
-                        lift.liftUp(),
-                        claw.openClaw(),
-                        lift.liftDown(),
-                        trajectoryActionCloseOut
+                        driveAndDetect.build()
                 )
         );
+        Action presetObelisk;
+        if (Constants.tagID == 22) {
+            presetObelisk = PGP.build();
+        } else {
+            presetObelisk = GPP_PPG.build();
+        }
+        Actions.runBlocking(presetObelisk);
     }
 }
